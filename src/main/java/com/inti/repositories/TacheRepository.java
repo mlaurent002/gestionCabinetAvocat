c1c0bcb04fb25fe43ef0f15c7cba31ba33a2d645
Birdarc <tanguy.frachon@free.fr>
2023-03-10 10:00:56 +0100

    tache back

---
+++ src/main/java/com/inti/repositories/TacheRepository.java
@@ -0,0 +1,13 @@
+package com.inti.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inti.entities.Tache;
+
+
+
+@Repository
+public interface TacheRepository extends JpaRepository<Tache, Long>{
+
+}
